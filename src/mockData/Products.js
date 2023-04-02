ff22764abcdf1478af9c76e4f4031b37c16cc5f4
Berkay Nayman <berkaynayman4@gmail.com>
2023-04-02 15:25:20 +0300

    Added color.js and Products.js at /mockData

---
+++ src/mockData/Products.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        _id: "1",
+        name: "Product Name 1",
+        image: "https://rukminim1.flixcart.com/image/612/612/xif0q/shoe/g/c/o/9-rockey-9-magnolia-white-original-imaggutpbvcczyhf.jpeg?q=70",
+        description: "Culpa pariatur labore irure nisi culpa consectetur ex Lorem.",
+        price: 100,
+        countInStock: 10,
+        rating: 3,
+        numReviews: 2
+    },
+    {
+        _id: "2",
+        name: "Product Name 2",
+        image: "https://rukminim1.flixcart.com/image/612/612/xif0q/shoe/g/c/o/9-rockey-9-magnolia-white-original-imaggutpbvcczyhf.jpeg?q=70",
+        description: "Culpa pariatur labore irure nisi culpa consectetur ex Lorem.",
+        price: 100,
+        countInStock: 10,
+        rating: 3,
+        numReviews: 2
+    },
+    {
+        _id: "3",
+        name: "Product Name 3",
+        image: "https://rukminim1.flixcart.com/image/612/612/xif0q/shoe/g/c/o/9-rockey-9-magnolia-white-original-imaggutpbvcczyhf.jpeg?q=70",
+        description: "Culpa pariatur labore irure nisi culpa consectetur ex Lorem.",
+        price: 100,
+        countInStock: 10,
+        rating: 3,
+        numReviews: 2
+    }
+]
+
+export default products
